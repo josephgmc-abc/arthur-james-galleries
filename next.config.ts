305a1a791bfc72a586159f0bb949029c3af9dc66
josephgmc <josephgmc@airasia.com>
2026-03-19 15:09:48 +0800

    fix: configure remote patterns for sanity and sothebys images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,26 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sothebys-md.brightspotcdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dam.sothebys.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.mixkit.co',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
